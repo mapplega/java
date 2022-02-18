bef99608eed2e5c93d316ba1ba119f930d1b058c
mapplega <mapplega@gmail.com>
2022-02-18 10:05:19 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let userInput;
+userInput = "samosa";
+if ( userInput === "burrito" ) {
+  $("#response").html("Brilliant choice!");
+} else {
+  $("#response").html("Don’t you want a burrito?");
+}
