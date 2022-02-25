dbdec3f6acc7914f2a127f0aebb9d5cb6d62306f
mapplega <mapplega@gmail.com>
2022-02-25 09:04:16 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
-let userInput;
+/*let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don’t you want a burrito?");
 }
+*/
+let makeABurrito;
+makeABurrito = function(beansVariable){
+  let beansResponse;
+  beansResponse = "You ordered " + beansVariable + " beans. Good choice!";
+  $("#response").html(beansResponse);
+};
+let blackBeans;
+blackBeans = "black";
+makeABurrito(blackBeans);
