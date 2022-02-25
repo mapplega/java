99c8ea9e530def1c1c8595f014186a11a99e23ff
mapplega <mapplega@gmail.com>
2022-02-25 09:28:23 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
-let userInput;
+/*let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don’t you want a burrito?");
 }
+*/
+
+let userInput;
+userInput = prompt("Do you think you are smart?", "Type your answer below.");
+if ( userInput === "yes" ) {
+  $("#response").html("Well aren't you arrogant.");
+} else {
+  $("response").html("You lack self-esteem.");
+}
+
+
 
 
 /*let makeABurrito;
